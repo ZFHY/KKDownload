b0e6197ac6d517cf9d23ac85f24bb9b7b4fb705c
chubao <>
2024-05-10 11:39:00 +0800

    44

--- Example/KKDownload/KKViewController.m
+++ Example/KKDownload/KKViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "KKViewController.h"
+#import "KKTestApi.h"
 
 @interface KKViewController ()
 
@@ -18,6 +19,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[KKTestApi new] testMethod];
+    [[KKTestApi new] testLog];
 }
 
 - (void)didReceiveMemoryWarning
