aaa1687afe96fa651873798d599406c99c520330
chubao <>
2024-05-10 18:10:54 +0800

    sdk 0.1.0

---
+++ KKDownload-0.1.0/ios/KKDownload.framework/Versions/A/Headers/KKTestApi.h
@@ -0,0 +1,19 @@
+//
+//  KKTestApi.h
+//  KKDownload
+//
+//  Created by chubao on 2024/5/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KKTestApi : NSObject
+
+-(void)testMethod;
+
+-(void)testLog;
+@end
+
+NS_ASSUME_NONNULL_END
